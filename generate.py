723e9a5e5db47bf5ec1ad805690a5887cb67b40d
gustav.hermansson <gustav.hermansson@afry.com>
2022-12-29 02:18:40 +0100

    formatting

--- generate.py
+++ generate.py
@@ -7,12 +7,12 @@ import numpy as np
 
 
 class Brownian(object):
-    def __init__(self, settings:Settings):
+    def __init__(self, settings: Settings):
         self.settings = settings
         self.result = Result(settings)
         self.current_time = self.settings.start_time
 
-    def catch_path_is_outside(self,old_pos):
+    def catch_path_is_outside(self, old_pos):
 
         x = old_pos.x - normal(self.settings.normal_mean, self.settings.normal_std)
         y = old_pos.y - normal(self.settings.normal_mean, self.settings.normal_std)
@@ -27,7 +27,9 @@ class Brownian(object):
             new_pos = geometry.Point(x, y)
         if not self.settings.boundary.contains(new_pos):
             self.catch_path_is_outside(old_pos)
-            raise Exception("Path is outside of boundary and could not change course correcytly. Tr again")
+            raise Exception(
+                "Path is outside of boundary and could not change course correcytly. Tr again"
+            )
 
         return new_pos
 
@@ -49,7 +51,10 @@ class Brownian(object):
         poly = self.settings.boundary
         box = poly.minimum_rotated_rectangle
         x, y = box.exterior.coords.xy
-        edge_length = (geometry.Point(x[0], y[0]).distance(geometry.Point(x[1], y[1])), geometry.Point(x[1], y[1]).distance(geometry.Point(x[2], y[2])))
+        edge_length = (
+            geometry.Point(x[0], y[0]).distance(geometry.Point(x[1], y[1])),
+            geometry.Point(x[1], y[1]).distance(geometry.Point(x[2], y[2])),
+        )
         length = max(edge_length)
         width = min(edge_length)
         self.length_modifier = max(length, width) * 0.01
@@ -59,16 +64,19 @@ class Brownian(object):
         boundary_exterior = self.settings.boundary.exterior
         does_intersect_with_boundary = not boundary_exterior.intersection(line).is_empty
         return does_intersect_with_boundary
+
     def generate_new_pos(self, old_pos: geometry.Point):
-        change_grazing_zone = np.random.randint(0,10000)/10000
+        change_grazing_zone = np.random.randint(0, 10000) / 10000
         if change_grazing_zone < self.settings.migration_factor:
             new_pos = self.find_random_point_in_polygon()
-            while (not self.settings.boundary.contains(new_pos)) or (self.line_intersects(old_pos, new_pos)):
+            while (not self.settings.boundary.contains(new_pos)) or (
+                self.line_intersects(old_pos, new_pos)
+            ):
                 new_pos = self.find_random_point_in_polygon()
 
         else:
-            delta_x = normal(0, 1)*self.length_modifier
-            delta_y = normal(0, 1)*self.length_modifier
+            delta_x = normal(0, 1) * self.length_modifier
+            delta_y = normal(0, 1) * self.length_modifier
             x = old_pos.x + delta_x
             y = old_pos.y + delta_y
 
@@ -91,7 +99,6 @@ class Brownian(object):
         self.step_in_time()
         return True
 
-
     def step_in_time(self):
         self.result.timestamps.append(self.result.settings.start_time)
         self.current_time += self.settings.timestep
@@ -105,7 +112,6 @@ class Brownian(object):
             found = False
             while not found:
                 found = self.generate_new_pos(self.result.positions[-1])
-
 
     def display_results(self):
         print(self.result)
@@ -123,124 +129,38 @@ class Brownian(object):
         res += f"start_pos: {self.result.positions[0]}\n"
         return res
 
+
 if __name__ == "__main__":
     polygon = [
-        [
-            17.995366850522515,
-            59.33057931698136
-        ],
-        [
-            17.995928949604263,
-            59.335739843849126
-        ],
-        [
-            18.006868244097205,
-            59.341264196549076
-        ],
-        [
-            18.02709688175287,
-            59.3386224723839
-        ],
-        [
-            18.046658391763145,
-            59.33444978561704
-        ],
-        [
-            18.056776175241623,
-            59.327353589628416
-        ],
-        [
-            18.04159950002463,
-            59.325848145401864
-        ],
-        [
-            18.029514369759312,
-            59.3282854978724
-        ],
-        [
-            18.025720200955107,
-            59.32613490182459
-        ],
-        [
-            18.029233320219163,
-            59.323123838704674
-        ],
-        [
-            18.03597850920434,
-            59.322908752559414
-        ],
-        [
-            18.039210578925463,
-            59.3214748101403
-        ],
-        [
-            18.03864847984366,
-            59.32061441565148
-        ],
-        [
-            18.028671221136022,
-            59.31910867289071
-        ],
-        [
-            18.02445547802074,
-            59.31989740360763
-        ],
-        [
-            18.016586090872067,
-            59.31982570157129
-        ],
-        [
-            18.014337694543713,
-            59.32061441565148
-        ],
-        [
-            18.016305041330554,
-            59.32154650869816
-        ],
-        [
-            18.01700766518306,
-            59.32204839436724
-        ],
-        [
-            18.01925606151144,
-            59.322621968915
-        ],
-        [
-            18.024174428479228,
-            59.32176160346384
-        ],
-        [
-            18.024596002791583,
-            59.32276536103984
-        ],
-        [
-            18.028390171595845,
-            59.32348231258868
-        ],
-        [
-            18.025017577102545,
-            59.325919834734236
-        ],
-        [
-            18.024174428479228,
-            59.32455771155588
-        ],
-        [
-            18.0223476064632,
-            59.32462940361063
-        ],
-        [
-            18.020942358758106,
-            59.32505955276281
-        ],
-        [
-            18.011386674361404,
-            59.325202934603595
-        ],
-        [
-            17.995366850522515,
-            59.33057931698136
-        ]
+        [17.995366850522515, 59.33057931698136],
+        [17.995928949604263, 59.335739843849126],
+        [18.006868244097205, 59.341264196549076],
+        [18.02709688175287, 59.3386224723839],
+        [18.046658391763145, 59.33444978561704],
+        [18.056776175241623, 59.327353589628416],
+        [18.04159950002463, 59.325848145401864],
+        [18.029514369759312, 59.3282854978724],
+        [18.025720200955107, 59.32613490182459],
+        [18.029233320219163, 59.323123838704674],
+        [18.03597850920434, 59.322908752559414],
+        [18.039210578925463, 59.3214748101403],
+        [18.03864847984366, 59.32061441565148],
+        [18.028671221136022, 59.31910867289071],
+        [18.02445547802074, 59.31989740360763],
+        [18.016586090872067, 59.31982570157129],
+        [18.014337694543713, 59.32061441565148],
+        [18.016305041330554, 59.32154650869816],
+        [18.01700766518306, 59.32204839436724],
+        [18.01925606151144, 59.322621968915],
+        [18.024174428479228, 59.32176160346384],
+        [18.024596002791583, 59.32276536103984],
+        [18.028390171595845, 59.32348231258868],
+        [18.025017577102545, 59.325919834734236],
+        [18.024174428479228, 59.32455771155588],
+        [18.0223476064632, 59.32462940361063],
+        [18.020942358758106, 59.32505955276281],
+        [18.011386674361404, 59.325202934603595],
+        [17.995366850522515, 59.33057931698136],
     ]
 
     settings = Settings(
@@ -249,11 +169,7 @@ if __name__ == "__main__":
         n_steps=500,
     )
     for i in range(10):
-        b = Brownian(settings = settings)
+        b = Brownian(settings=settings)
         b.run()
         b.result.to_csv(f"faketimeseriesdata{i}.csv")
         b.plot_result_path()
-
-
-
-
--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from typing import List
 import csv
 
+
 @dataclass
 class Settings:
     boundary: geometry.Polygon
@@ -12,27 +13,36 @@ class Settings:
     migration_factor = 0.005
     start_time: datetime = datetime.now()
 
+
 class Result:
     def __init__(self, settings: Settings):
         self.positions: List[geometry.Point] = []
         self.timestamps: List[datetime] = []
         self.settings = settings
         self._timedelta = self.settings.timestep
-        self.end_time = self.settings.start_time - (self.settings.n_steps * self._timedelta)
+        self.end_time = self.settings.start_time - (
+            self.settings.n_steps * self._timedelta
+        )
 
-    def to_csv(self, filename:str):
+    def to_csv(self, filename: str):
         ## save positions and timestamps to csv file
-        result_dict = {"x": [p.x for p in self.positions],
-                        "y": [p.y for p in self.positions],
-                        "timestamp": self.timestamps}
-        _range = range(len(self.positions)-1)
+        result_dict = {
+            "x": [p.x for p in self.positions],
+            "y": [p.y for p in self.positions],
+            "timestamp": self.timestamps,
+        }
+        _range = range(len(self.positions) - 1)
 
         cols = []
         for i in _range:
-            row = [result_dict["x"][i], result_dict["y"][i], result_dict["timestamp"][i]]
+            row = [
+                result_dict["x"][i],
+                result_dict["y"][i],
+                result_dict["timestamp"][i],
+            ]
             cols.append(row)
 
-        with open(filename, 'w') as f:
+        with open(filename, "w") as f:
             w = csv.writer(f)
             w.writerow(result_dict.keys())
             w.writerows(cols)
@@ -41,11 +51,16 @@ class Result:
         try:
             total_time = self.settings.start_time - self.end_time
             res = ""
-            for i in range(len(self.positions)-1):
-                res += str(self.positions[i].x) +"   "+ str(self.positions[i].y) +"   "+ str(self.timestamps[i]) + "\n"
+            for i in range(len(self.positions) - 1):
+                res += (
+                    str(self.positions[i].x)
+                    + "   "
+                    + str(self.positions[i].y)
+                    + "   "
+                    + str(self.timestamps[i])
+                    + "\n"
+                )
             res += f" {self.settings.n_steps} steps Time interval {total_time} from {self.end_time} to {self.settings.start_time}"
         except Exception as e:
             res = "Error: " + str(e)
         return res
-
-
